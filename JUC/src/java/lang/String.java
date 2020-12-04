5d10fb19c51b0f6afb37062693d4fe30d7d945f3
lhf <2744613815@qq.com>
2020-12-04 15:49:28 +0800

    retrogress 1

---
+++ JUC/src/java/lang/String.java
@@ -0,0 +1,18 @@
+package java.lang;
+
+/**
+ * @author LHF
+ * @date 2020/11/28 22:53
+ *
+ * 双亲委派机制
+ * 沙箱安全机制
+ *
+ */
+public class String {
+
+    //在类 java.lang.String 中找不到 main 方法
+    public static void main(String[] args){
+        System.out.println("***********");
+    }
+
+}
